e92a1485c15de405e785ff8b8ad9605ce94e28e7
Mukund Madhav <mukund.madhav@kalvium.community>
2023-08-25 22:55:32 +0530

    groove grocery done

---
+++ app.js
@@ -0,0 +1,10 @@
+var buyBtn = document.getElementById("buy-now")
+var text = document.getElementById("thanks-text")
+
+buyBtn.onclick=()=>{
+    text.style.display= "block"
+    buyBtn.style.display= "none"
+}
+// document.getElementById('buy-now').onclick=()=>(
+//     document.getElementById
+// )
